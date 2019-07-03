884cde66637600f510578edb95c935c084a45522
drepkovsky <dominik.repkovsky@gmail.com>
2019-07-03 17:42:44 +0200

    Scena vytvorena, refaktor oproti povodnej verzi hadzania, hadzanie je funkcne, treba uz len doladit premenne

--- ThrowIT/Assets/Scripts/InputHandler.cs
+++ ThrowIT/Assets/Scripts/InputHandler.cs
@@ -4,18 +4,20 @@ using System.Collections;
 public class InputHandler : MonoBehaviour
 {
 
-    public Vector2 tapPosition, releasePosition;
+    Vector2 lastPosition, releasePosition;
 
-    public Throwable activeThrowable;
+    Throwable activeThrowable;
 
     public LayerMask throwableLayerMask;
 
-    void Start()
-    {
+    public float zVelocity = 10;
+    [Range(1,20)]
+    public float throwMultiplier = 5;
 
-    }
+    float lastTime = 0;
+    public float minimalSwipeVelocity;
 
-    void Update()
+    void FixedUpdate()
     {
         HandleInput();
     }
@@ -27,49 +29,80 @@ public class InputHandler : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            tapPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
             //hladame throwable objekt
             activeThrowable = FindThrowable();
 
-            //ak sme ho nasli tak mu zavolame Attach, vypneme mu gravitaciu
+            //ak sme ho nasli tak mu zavolame Attach
             if (activeThrowable != null)
             {
                 activeThrowable.Attach();
+
+                lastPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                lastTime = Time.time;
             }
+
 
         }
         else if (Input.GetMouseButton(0))
         {
+            lastPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
             //ked pustime palec, tak sa zavola Dettach objektu throwable ktory mu zapne gravitaciu a spadne naspat nazem
             //zistujeme pohyb v priestore cez x,y poziciu mysi a z poziciu objektu relativne na kameru
             if (activeThrowable != null)
             {
-                Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * Mathf.Abs(Camera.main.transform.position.z - activeThrowable.transform.position.z));
-                position.z  = activeThrowable.transform.position.z;
 
-                activeThrowable.followPoint = position;
+                Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * Mathf.Abs(Camera.main.transform.position.z - activeThrowable.transform.position.z));
+                position.z = activeThrowable.transform.position.z;
+
+                Vector3 dir = position - activeThrowable.transform.position;
+
+                float velocityMultiplier = dir.magnitude / Time.deltaTime;
+
+                Vector3 velocity = dir * velocityMultiplier;
+
+                activeThrowable.GetRigidbody().velocity = velocity;
+
+
+                lastPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+                lastTime = Time.time;
             }
+
 
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            releasePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-            //ked pustime palec, tak sa zavola Dettach objektu throwable ktory mu zapne gravitaciu a spadne naspat nazem
+            //ked pustime palec, tak sa zavola Dettach objektu 
             if (activeThrowable != null)
             {
-                activeThrowable.Dettach();
+                float deltaTime = Time.time - lastTime;
+                releasePosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
+                if ((lastPosition - releasePosition).magnitude / deltaTime > minimalSwipeVelocity)
+                {
+
+                    Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * Mathf.Abs(Camera.main.transform.position.z - activeThrowable.transform.position.z));
+                    position.z = activeThrowable.transform.position.z;
+
+                    Vector3 dir = position - activeThrowable.transform.position;
+
+                    Vector3 velocity = dir.normalized * throwMultiplier;
+
+                    velocity.z = zVelocity;
+
+                    activeThrowable.GetRigidbody().velocity = velocity;
+                }
+
+                activeThrowable.Dettach();
+                activeThrowable = null;
             }
 
-            activeThrowable = null;
         }
 
 
     }
 
-//najdeme objekty ktore maju tag Throwable, iba tieto objekty bude mozne chytit a hodit
+    //najdeme objekty ktore maju tag Throwable, iba tieto objekty bude mozne chytit a hodit
     public Throwable FindThrowable()
     {
 
--- ThrowIT/Assets/Scripts/Throwable.cs
+++ ThrowIT/Assets/Scripts/Throwable.cs
@@ -8,15 +8,9 @@ public class Throwable : MonoBehaviour
     //tag ktory automaticky nastavime vsetkym objektom s tymto skriptom
     public static string THROWABLE_TAG = "Throwable";
 
-    //bod ktory sme nastavili cez input handler tomuto objektu, tento bod budeme nasledovat linearne rychlostou followspeed
-    [HideInInspector]
-    public Vector3 followPoint = Vector3.zero;
-    public int followSpeed = 15;
-
     Rigidbody rb;
 
-
-
+    bool attached = false;
 
 
     void Start()
@@ -27,47 +21,25 @@ public class Throwable : MonoBehaviour
 
     }
 
-    void Update()
-    {
-    }
 
-    //pohyb rigidbodies robievame vo fixedUpdate lebo fyzika sa pocita kazdy FixedUpdate a nie update
-    void FixedUpdate()
-    {
-        if (followPoint != Vector3.zero)
-        {
-            Follow();
-        }
-    }
-
-
-    private void Follow()
-    {
-        //ak sa pozicia objektu nerovna followPointu tak sa k nemu priblizime funkciou lerp
-        if (followPoint != transform.position)
-        {
-            transform.position = Vector3.Lerp(transform.position, followPoint, followSpeed * Time.deltaTime);
-        }
-
-    }
-
-    public void Throw()
-    {
-
-    }
-
-    //objekt sme chytili ,vypneme gravitaciu
     public void Attach()
     {
-        rb.useGravity = false;
+        attached = true;
     }
-
-    //objekt sme pustili ,zapneme gravitaciu
 
     public void Dettach()
     {
-        rb.useGravity = true;
-        followPoint = Vector3.zero;
+        attached = false;
     }
 
+
+    public Rigidbody GetRigidbody()
+    {
+        return rb;
+    }
+
+    public bool IsAttached()
+    {
+        return attached;
+    }
 }
